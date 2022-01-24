d8bbeadc2f2f21edcf0f910e3722cba7a6a06f87
AugustoJmnz <2002010@utrivieramaya.edu.mx>
2022-01-24 12:39:45 -0500

    Weather

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,15 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+  
+path: "/Tiempo",
+name: "Tiempo",
+// route level code-splitting
+// this generates a separate chunk (about.[hash].js) for this route
+// which is lazy-loaded when the route is visited.
+component: () =>
+  import(/* webpackChunkName: "tiempo */ "../views/Tiempo.vue"),
+},
 ];
 
 const router = new VueRouter({
---
+++ src/views/Tiempo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-if="result" class="tiempo">
+  <h1><p><b>{{result.name}}</b></p></h1>
+ 
+    <img :src="'http://openweathermap.org/img/w/'+result.weather[0].icon+'.png' " alt=" ">
+    <p>{{ result.weather[0].description }}</p>
+    <p>Weather: {{ result.main.weather }}</p>
+    <p>Degrees: {{ result.wind.deg }}</p>
+    <p>Pressure: {{ result.temp.pressure }}</p>
+    <p>Humidity: {{ result.temp.humidity }}</p>
+    
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: () => ({
+    result: null,
+  }),
+  created() {
+      axios
+        .get(
+            "https://api.openweathermap.org/data/2.5/weather?q=playa%20del%20carmen&APPID=939aae2cc565a6f67803922eca539920")
+        .then((result) => {
+          this.result = result.data;
+          console.log(result.data);
+        });
+  }
+};
+</script>
